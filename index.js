f690d7d7a4cf3b7aa061dcff4b3f3b0f17bc271e
zuesuep <kimsanz@gmail.com>
2023-04-17 09:14:48 +0800

    renamed:    app.js -> index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+import * as dotenv from "dotenv";
 import express from "express";
 import dateFunc from "./public/js/date.js";
 import mongoose from "mongoose";
 import _ from "lodash";
 
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-const uri = "mongodb://127.0.0.1:27017/todolistDB";
-// "mongodb+srv://uepzues:sdWkzEvqkb234xh@zuesuep.8bkzlbx.mongodb.net/todolistDB";
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", `ejs`);
 
-mongoose.connect(uri);
+mongoose.connect(process.env.MONGO_URI);
 
+// mongoose.set("strictQuery", false);
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.log(err);
+//     // process.exit(1);
+//   }
+// };
 
 //schema
 const itemsSchema = {
@@ -136,6 +147,12 @@ app.post("/delete", (req, res) => {
     ).then(res.redirect("/" + listName));
   }
 });
+
+// connectDB().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server started on ${port}`);
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
